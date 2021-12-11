c708f2f68fdacb310228bf5cedd8a4dc52f7c003
Mingyu Li <lmytime@hotmail.com>
2021-12-11 23:41:19 +0800

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,5 @@
+cd ~
+mkdir -p .vim && cd .vim
+curl -0 vimrc https://raw.githubusercontent.com/lmytime/my-vim/main/vimrc
+mkdir colors && cd colors
+curl -o molokai.vim https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim
